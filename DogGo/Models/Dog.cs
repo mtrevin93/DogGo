dec36b9ba8b47c44bf459e16990601ed1a411b1d
Michael Trevino <mtrevin93@gmail.com>
2021-09-26 15:50:24 -0400

    added data annotations for required field & input field formatting

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace DogGo.Models
 {
     public class Dog
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Breed { get; set; }
         public Owner Owner { get; set; }
         public String Notes { get; set; }
+        [DisplayName("Image URL")]
         public string ImageUrl { get; set; }
+        [DisplayName("Owner")]
+        [Required]
         public int OwnerId { get; set; }
     }
 }
